516705904cc9bd8ed4b95f0b3bde231513c4e6b1
Paul Ptichkin <m1rage.nnov@gmail.com>
2022-04-03 16:41:07 +0300

    1 commitda

--- src/guru/qa/Main.java
+++ src/guru/qa/Main.java
@@ -3,6 +3,6 @@ package guru.qa;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hi qa!!!!");
+        System.out.println("hi qASasda!!!!");
     }
 }
